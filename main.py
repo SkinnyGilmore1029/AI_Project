80cd6306371461f286553a2fa615c2e9fb9c6aca
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-06-16 07:29:59 -0500

    getting user input

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+user_input = sys.argv[1]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                          contents ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+if user_input:
+    response = client.models.generate_content(model="gemini-2.0-flash-001",
+                                              contents =user_input)
+else:
+    print("Please provide a model name as a command-line argument.")
+    sys.exit(1)
 
 token_count = response.usage_metadata.prompt_token_count
 cand_token_count = response.usage_metadata.candidates_token_count
